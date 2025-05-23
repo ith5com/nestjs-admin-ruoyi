f47b0f59304634ef0125efcd94244bd5166229b7
ith5 <ith5cn@163.com>
2025-05-23 15:12:18 +0800

    守卫应用

--- src/modules/system/user/user.controller.ts
+++ src/modules/system/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserService } from './services/user.service';
 import { CreateUserDto } from './dto/user.dto';
+import { JwtSystemGuardGuard } from 'src/common/guards/auth/jwt-system-auth.guard';
 
 @ApiTags('系统用户')
+@UseGuards(JwtSystemGuardGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}

a4ac927e7e6c3bab91fc9fa8d19a9fbe2ce4c9d4
ith5 <ith5cn@163.com>
2025-05-25 22:58:56 +0800

    加上system

--- src/modules/system/system.module.ts
+++ src/modules/system/system.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { MenuModule } from './menu/menu.module';
-
+import { RouterModule } from '@nestjs/core';
 @Module({
-  imports: [UserModule, RoleModule, MenuModule],
+  imports: [
+    UserModule,
+    RoleModule,
+    MenuModule,
+    RouterModule.register([
+      {
+        path: 'system',
+        module: SystemModule,
+        children: [UserModule, RoleModule, MenuModule],
+      },
+    ]),
+  ],
 })
 export class SystemModule {}
